f338d421dcee8abbc390fae9446ac6cc9ba78a54
manasathalanki <thalankimanasa2520@gmail.com>
2024-10-03 12:46:30 +0530

    allowing urls without authentication

--- src/main/java/com/example/youtube_learning/config/SecurityConfig.java
+++ src/main/java/com/example/youtube_learning/config/SecurityConfig.java
@@ -21,11 +21,12 @@ public class SecurityConfig {
 	@Bean
 //	@Order(SecurityProperties.BASIC_AUTH_ORDER)
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests
-				.anyRequest().authenticated());
+		http.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll().anyRequest().authenticated());
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
+		http.csrf(csrf -> csrf.disable());
 		return http.build();
 	}
 
@@ -33,12 +34,12 @@ public class SecurityConfig {
 //	public User(String username, String password, boolean enabled, boolean accountNonExpired,
 //	boolean credentialsNonExpired, boolean accountNonLocked,
 //	Collection<? extends GrantedAuthority> authorities)
-		
+
 	@Bean
 	public UserDetailsService userDetailsService() {
 		UserDetails user1 = User.withUsername("user1").password("{noop}password").roles("USER").build();
 		UserDetails user2 = User.withUsername("user2").password("{noop}password").roles("ADMIN").build();
-		return new InMemoryUserDetailsManager(user1,user2);
+		return new InMemoryUserDetailsManager(user1, user2);
 	}
 
 }
