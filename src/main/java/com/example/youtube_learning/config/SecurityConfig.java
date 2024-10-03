002f3448ba76749c35f15efd3050b1dc1ea1b540
manasathalanki <thalankimanasa2520@gmail.com>
2024-10-03 13:49:25 +0530

    replaced inmemory with jdbc details manager

--- src/main/java/com/example/youtube_learning/config/SecurityConfig.java
+++ src/main/java/com/example/youtube_learning/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.example.youtube_learning.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,12 +14,17 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 //@EnableMethodSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	DataSource dataSource;
 
 	@Bean
 //	@Order(SecurityProperties.BASIC_AUTH_ORDER)
@@ -37,9 +45,12 @@ public class SecurityConfig {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
+		JdbcUserDetailsManager manager =new JdbcUserDetailsManager(dataSource);
 		UserDetails user1 = User.withUsername("user1").password("{noop}password").roles("USER").build();
 		UserDetails user2 = User.withUsername("user2").password("{noop}password").roles("ADMIN").build();
-		return new InMemoryUserDetailsManager(user1, user2);
+		manager.createUser(user2);
+		manager.createUser(user1);
+		return manager;
 	}
 
 }
